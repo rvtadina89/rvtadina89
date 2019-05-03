46a60a72e55cdadbd99627c7cf4c957aa5a40f78
rvtadina89 <rvtadina89@go.byuh.edu>
2019-05-03 04:18:31 -1000

    Initial Commit

---
+++ Concentration2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration2
+//
+//  Created by Arvie Tadina on 3/5/19.
+//  Copyright © 2019 Arvie Tadina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Concentration2Tests/Concentration2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Concentration2Tests.swift
+//  Concentration2Tests
+//
+//  Created by Arvie Tadina on 3/5/19.
+//  Copyright © 2019 Arvie Tadina. All rights reserved.
+//
+
+import XCTest
+@testable import Concentration2
+
+class Concentration2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
